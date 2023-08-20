ca0b36cf64c344a8855812d51188fa51096de9d6
Admin <desk.atharv16@gmail.com>
2023-08-20 09:46:47 -0700

    1st commit

---
+++ app/src/main/java/com/class_sync/HomeScreen.java
@@ -0,0 +1,90 @@
+package com.class_sync;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.google.android.material.card.MaterialCardView;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+
+public class HomeScreen extends AppCompatActivity {
+    MeowBottomNavigation bottomNavigation;
+MaterialCardView assignments,attendance,groupChatting,askChatGpt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        bottomNavigation=findViewById(R.id.bottomNavigation);
+        bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.baseline_home_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.baseline_chat_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.baseline_hub_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(4, R.drawable.baseline_person_24));
+        bottomNavigation.show(1,true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame,new HomeFragment()).commit();
+    bottomNavigation.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+    @Override
+    public Unit invoke(MeowBottomNavigation.Model model) {
+
+        switch (model.getId())
+        {
+            case 1:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame,new HomeFragment()).commit();
+                break;
+            case 2:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame,new MessengerFragment()).commit();
+
+                break;
+            case 3:
+                Toast.makeText(HomeScreen.this, "UpComing Fragment", Toast.LENGTH_SHORT).show();
+                break;
+            case 4:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame,new UserFragment()).commit();
+
+                break;
+        }
+        return null;
+    }
+});
+
+//        attendance=findViewById(R.id.TrackAttendance_CardView);
+//        groupChatting=findViewById(R.id.GroupChatting_CardView);
+//        assignments=findViewById(R.id.Assignments_CardView);
+//        askChatGpt=findViewById(R.id.AskChatGpt_CardView);
+//
+//        attendance.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(HomeScreen.this, "Track Attendance", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        groupChatting.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(HomeScreen.this, "Group Chatting", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        assignments.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(HomeScreen.this, "Assignments", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        askChatGpt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(HomeScreen.this, "Ask ChatGpt", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//
+
+    }
+}
---
+++ app/src/main/java/com/class_sync/MainActivity.java
@@ -0,0 +1,60 @@
+package com.class_sync;
+
+import android.animation.Animator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView t1;
+    LottieAnimationView l1;
+    int i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        l1 = findViewById(R.id.logoAnim);
+        t1 = findViewById(R.id.logoName);
+        Animation m1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.logoanim);
+        Animation m2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.logoanim2);
+        l1.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(@NonNull Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(@NonNull Animator animator) {
+//
+                Intent intent = new Intent(getApplicationContext(), HomeScreen.class);
+                startActivity(intent);
+                finish();
+            }
+
+
+
+            @Override
+            public void onAnimationCancel(@NonNull Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(@NonNull Animator animator) {
+
+            }
+        });
+
+
+    }
+}
