283ad18ffc969f32529693cbe5b6f7c6229b6623
Admin <desk.atharv16@gmail.com>
2023-12-03 21:26:18 +0530

    1st commit

---
+++ app/src/main/java/Home_Fragments/Msbte_Resources_SUB_Fragment.java
@@ -0,0 +1,23 @@
+package Home_Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.class_sync.R;
+
+public class Msbte_Resources_SUB_Fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ViewGroup root=(ViewGroup) inflater.inflate(R.layout.fragment_msbte__resources__s_u_b_, container, false);
+
+        return root;
+
+    }
+}
