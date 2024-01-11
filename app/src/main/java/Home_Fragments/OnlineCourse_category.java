a064af2e355786ee191d2d7bc245b0b382656989
Admin <desk.atharv16@gmail.com>
2024-01-11 21:42:40 +0530

    1st commit

--- app/src/main/java/Home_Fragments/OnlineCourse_category.java
+++ app/src/main/java/Home_Fragments/OnlineCourse_category.java
@@ -1,25 +1,109 @@
 package Home_Fragments;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.core.view.ViewCompat;
 import androidx.fragment.app.Fragment;
 
+import android.transition.Transition;
+import android.transition.TransitionInflater;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.class_sync.R;
+
+import java.util.Random;
 
 
 public class OnlineCourse_category extends Fragment {
 
+    ImageView img;
+    TextView text1,text2,text3;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         ViewGroup root =(ViewGroup) inflater.inflate(R.layout.fragment_online_course_category, container, false);
+    img = root.findViewById(R.id.CourseCategory_img);
+    text1 = root.findViewById(R.id.text1);
+    text2 = root.findViewById(R.id.text2);
+    text3 = root.findViewById(R.id.text3);
+
+        Random r =new Random();
+        int random_num = r.nextInt(2)+1;
 
 
+        Animation animation_right= AnimationUtils.loadAnimation(getActivity(),R.anim.category_text);
+        Animation animation_left= AnimationUtils.loadAnimation(getActivity(),R.anim.category_text_left);
+
+        if(random_num == 2){
+            text1.startAnimation(animation_right);
+            text2.startAnimation(animation_right);
+            text3.startAnimation(animation_right);
+        }else {
+            text1.startAnimation(animation_left);
+            text2.startAnimation(animation_left);
+            text3.startAnimation(animation_left);
+        }
+
+
+
+
+        Bundle arguments = getArguments();
+        if (arguments != null) {
+            int resourceId = arguments.getInt("imageViewId", -1);
+            String Img_transition_name = arguments.getString("transitionName","");
+//            String text_transition_name = arguments.getString("Text_transitionName","");
+
+            // Do something with the ImageView ID
+            if (resourceId != -1) {
+               img.setImageResource(resourceId);
+                ViewCompat.setTransitionName(img,Img_transition_name);
+//                ViewCompat.setTransitionName(textView,text_transition_name);
+                // Now you have the reference to the ImageView in Fragment B
+            }
+        }
+
+        //The code written below is used to make a shared Element transition in fragment
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setSharedElementEnterTransition(TransitionInflater.from(getContext()).inflateTransition(android.R.transition.move));
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getActivity().getWindow().getSharedElementEnterTransition().addListener(new Transition.TransitionListener() {
+                @Override
+                public void onTransitionStart(Transition transition) {
+                    // Not needed, but you can add custom behavior here
+                }
+
+                @Override
+                public void onTransitionEnd(Transition transition) {
+                    // Start postponed enter transition when the shared element is ready
+                    startPostponedEnterTransition();
+                }
+
+                @Override
+                public void onTransitionCancel(Transition transition) {
+                    // Not needed, but you can add custom behavior here
+                }
+
+                @Override
+                public void onTransitionPause(Transition transition) {
+                    // Not needed, but you can add custom behavior here
+                }
+
+                @Override
+                public void onTransitionResume(Transition transition) {
+                    // Not needed, but you can add custom behavior here
+                }
+            });
+        }
 
         return root;
     }
--- app/src/main/java/Online_Courses/OnlineCourse_Home_Fragment.java
+++ app/src/main/java/Online_Courses/OnlineCourse_Home_Fragment.java
@@ -1,17 +1,22 @@
 package Online_Courses;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.cardview.widget.CardView;
 import androidx.core.app.ActivityOptionsCompat;
 import androidx.core.view.ViewCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.transition.TransitionInflater;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 
 import com.class_sync.R;
@@ -23,7 +28,10 @@ public class OnlineCourse_Home_Fragment extends Fragment {
 
 
     CardView Design_Category,Development_Category,Marketing_Category,Business_Category;
-    ImageView img ;
+
+    ImageView Development_img,Design_img,Marketing_img,Business_img;
+    TextView t1;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -33,25 +41,95 @@ public class OnlineCourse_Home_Fragment extends Fragment {
         Development_Category = root.findViewById(R.id.Development_Category);
         Marketing_Category = root.findViewById(R.id.Marketing_Category);
         Business_Category = root.findViewById(R.id.Business_Category);
-        img = root.findViewById(R.id.develop_img);
+
+        t1=root.findViewById(R.id.t1);
+
+        Development_img=root.findViewById(R.id.develop_img);
+        Design_img=root.findViewById(R.id.Design_img);
+        Marketing_img=root.findViewById(R.id.Marketing_img);
+        Business_img=root.findViewById(R.id.Business_img);
 
 
+        Business_Category.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OnlineCourse_category fragmentB = new OnlineCourse_category();
+                Bundle bundle = new Bundle();
+                bundle.putInt("imageViewId", R.drawable.business); // Pass the ImageView ID
+                bundle.putString("transitionName","business");
+//                bundle.putString("Text_transitionName","txt");
+                fragmentB.setArguments(bundle);
 
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frame, fragmentB);
+                fragmentTransaction.addSharedElement(Business_img,"business").addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+Marketing_Category.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        OnlineCourse_category fragmentB = new OnlineCourse_category();
+        Bundle bundle = new Bundle();
+        bundle.putInt("imageViewId", R.drawable.marketing); // Pass the ImageView ID
+        bundle.putString("transitionName","marketing");
+        fragmentB.setArguments(bundle);
+
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame, fragmentB);
+        fragmentTransaction.addSharedElement(Marketing_img,"marketing").addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+});
+Design_Category.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        OnlineCourse_category fragmentB = new OnlineCourse_category();
+        Bundle bundle = new Bundle();
+        bundle.putInt("imageViewId", R.drawable.design); // Pass the ImageView ID
+        bundle.putString("transitionName","design");
+        fragmentB.setArguments(bundle);
+
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame, fragmentB);
+        fragmentTransaction.addSharedElement(Design_img,"design").addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+});
         Development_Category.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                ActivityOptionsCompat optionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(getActivity(),img, ViewCompat.getTransitionName(img));
-                getActivity().getSupportFragmentManager().beginTransaction().addToBackStack("").replace(R.id.frame, new OnlineCourse_category()).commit();
-//                startActivity(new Intent(getActivity(), OnlineCourseView.class),optionsCompat.toBundle());
+
+                OnlineCourse_category fragmentB = new OnlineCourse_category();
+                Bundle bundle = new Bundle();
+                bundle.putInt("imageViewId", R.drawable.develop);
+                bundle.putString("transitionName","example");// Pass the ImageView ID
+                fragmentB.setArguments(bundle);
 
 
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction().addSharedElement(Development_img,"example");
+                fragmentTransaction.replace(R.id.frame, fragmentB);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
 
 
+//                getActivity().getSupportFragmentManager().beginTransaction().addSharedElement(Development_img,"example").replace(R.id.frame,new OnlineCourse_category()).addToBackStack(null).commit();
 
             }
         });
 
+
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setSharedElementReturnTransition(TransitionInflater.from(getContext()).inflateTransition(android.R.transition.move));
+        }
         return root;
     }
+
 
 }
