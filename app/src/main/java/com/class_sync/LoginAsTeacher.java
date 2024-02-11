edb1cc38eb2060eedcfef8a28d3015d29fc1b7b0
Admin <desk.atharv16@gmail.com>
2024-02-11 23:07:10 +0530

    updated on 11/2/2024

--- app/src/main/java/com/class_sync/LoginAsTeacher.java
+++ app/src/main/java/com/class_sync/LoginAsTeacher.java
@@ -1,5 +1,6 @@
 package com.class_sync;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
@@ -9,13 +10,30 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
 
 import org.w3c.dom.Text;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginAsTeacher extends AppCompatActivity {
 EditText PhoneNumber_EditText,Password_EditText,OTP_EditText;
 TextView SendOTP,LoginAsStudent;
 CardView login;
+FirebaseAuth auth = FirebaseAuth.getInstance();
+Long timeoutSeconds = 60L;
+String phoneNumber;
+String verificationCode;
+PhoneAuthProvider.ForceResendingToken resendingToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,24 +47,24 @@ CardView login;
         SendOTP = findViewById(R.id.sendOtp);
         login = findViewById(R.id.TeacherLogin_cardView);
 
-
+        PhoneNumber_EditText.setText("+91  ");
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),TeacherHomeScreen.class));
+                String enteredOTP = OTP_EditText.getText().toString();
+                PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationCode,enteredOTP);
+                signIn(credential);
+//                startActivity(new Intent(getApplicationContext(),TeacherHomeScreen.class));
             }
         });
         SendOTP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 OTP_EditText.setVisibility(View.VISIBLE);
+                sendOtp(PhoneNumber_EditText.getText().toString()  ,false);
             }
         });
-
-
-
-
 
     LoginAsStudent.setOnClickListener(new View.OnClickListener() {
         @Override
@@ -55,5 +73,53 @@ CardView login;
             finish();
         }
     });
+    }
+   void sendOtp(String phoneNumber,Boolean isResend){
+       PhoneAuthOptions.Builder builder =
+               PhoneAuthOptions.newBuilder(auth)
+                       .setPhoneNumber(PhoneNumber_EditText.getText().toString())
+                       .setTimeout(timeoutSeconds, TimeUnit.SECONDS)
+                       .setActivity(this)
+                       .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                           @Override
+                           public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                               signIn(phoneAuthCredential);
+                           }
+
+                           @Override
+                           public void onVerificationFailed(@NonNull FirebaseException e) {
+                               Toast.makeText(LoginAsTeacher.this, "OTP verification failed", Toast.LENGTH_SHORT).show();
+                           }
+
+                           @Override
+                           public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                               super.onCodeSent(s, forceResendingToken);
+                               verificationCode = s;
+                               resendingToken = forceResendingToken;
+                               Toast.makeText(LoginAsTeacher.this, "OTP sent Successfully", Toast.LENGTH_SHORT).show();
+                           }
+                       });
+       if(isResend)
+       {
+           PhoneAuthProvider.verifyPhoneNumber(builder.setForceResendingToken(resendingToken).build());
+       }
+       else {
+           PhoneAuthProvider.verifyPhoneNumber(builder.build());
+       }
+    }
+
+    private void signIn(PhoneAuthCredential phoneAuthCredential) {
+        auth.signInWithCredential(phoneAuthCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    startActivity(new Intent(getApplicationContext(),TeacherHomeScreen.class));
+                 }
+                else {
+                    Toast.makeText(LoginAsTeacher.this, "OTP verification Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
